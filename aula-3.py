42ef1338b6995d4dc75454df523a32bb5e976c67
Eduardo <eduardo.ramos@mail.uft.edu.br>
2024-08-28 19:15:59 -0300

    Aula 3

---
+++ aula-3.py
@@ -0,0 +1,8 @@
+class MinhaClasse:
+atributo_publico = Nome
+     __atributo_privado = 5
+     def __init__(self):
+             self.atributo_publico = 10
+
+     def metodo(self):
+             return
